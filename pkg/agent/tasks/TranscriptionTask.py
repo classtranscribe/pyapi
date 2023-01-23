6fc32fe44b9b42210a377aa8111a9e010e9e1d85
atharvanaik <atharvanaik10@gmail.com>
2023-01-22 22:08:38 -0600

    Add code skeleton

--- pkg/agent/tasks/TranscriptionTask.py
+++ pkg/agent/tasks/TranscriptionTask.py
@@ -1,14 +1,19 @@
 import time
-
-from .AbstractTask import AbstractTask
+import json
+import os
+import requests
+import whisper
+from .AbstractTask import AbstractTask, TaskNames
 
 # no internal dependencies
-
 
 class TranscriptionTask(AbstractTask):
     @staticmethod
     def get_name():
         return "TranscriptionTask"
+
+    def transcribe(self, video_id, video, language, model):
+        
 
     def run_task(self, body, emitter):
         video_id = body['video_id']
@@ -18,19 +23,28 @@ class TranscriptionTask(AbstractTask):
         if force.lower() in ('true', '1', 't'):
             # TODO: delete existing captions and wipe transcription status
             return
-
         else:
             # TODO: short-circuit if transcription already exists
             return
+                # TODO: Fetch video by id (to get phrases?)
+        
+        parameters = body.get('TaskParameters', {})
+        # TODO check if I can get custom params
+        language = parameters.get('language', 'en')
 
-        # TODO: Fetch video by id (to get phrases?)
-        # video_id = body['video_id']
+        model = parameters.get('model', 'base')
+        self.logger.info(' [%s] Transcription started on videoId=%s...' % (video_id, video_id))
 
-        # TODO: Attempt 10 times?
+        # Fetch video metadata by id to get path
+        video = self.get_video(video_id=video_id)
 
-        # TODO: Set source language and target languages
+        if video is None:
+            self.logger.error(' [%s] TranscriptionTask FAILED to fetch video with videoId=%s' % (video_id, video_id))
+            return
 
-        # ExampleTask just waits for 12 seconds
-        # time.sleep(5)
-        # self.logger.info(' [.] TranscriptionTask completed!')
+        transcription = self.transcribe(video_id, video, language, model)
+
+
+
+
 
